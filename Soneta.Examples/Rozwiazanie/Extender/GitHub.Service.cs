7bef6d78db5124e3c2568af1466717e8e4e2d860
Michał Domeredzki <michal.d85@gmail.com>
2016-10-06 13:23:35 +0200

    test

--- Soneta.Examples/Rozwiazanie/Extender/GitHub.Service.cs
+++ Soneta.Examples/Rozwiazanie/Extender/GitHub.Service.cs
@@ -16,6 +16,7 @@ namespace Soneta.Examples.Rozwiazanie.Extender
         //22
         // 33
         //44
+        //55
        
         private const string RepoName = "angular/angular";
         private const string GitHubApiUrl = "https://api.github.com/repos/{0}/commits";
