8f98922cfeb4ed8c38edbad20c1fdf936e784c3b
Michał Domeredzki <michal.d85@gmail.com>
2016-10-06 21:05:15 +0200

    dodane folder dla przykladow

--- Soneta.Examples/Rozwiazanie/Extender/Statystki.GitHub.cs
+++ Soneta.Examples/Rozwiazanie/Extender/Statystki.GitHub.cs
@@ -1,10 +1,19 @@
 ﻿#define Rozwiazanie
 
 using System;
+using Soneta.Business.Licence;
 using Soneta.Business.UI;
 using Soneta.Examples.Rozwiazanie.Extender;
 
 #if Rozwiazanie
+
+[assembly: FolderView("Soneta.Examples",
+    Priority = 10,
+    Description = "Przykłady implementacji enova365",
+    BrickColor = FolderViewAttribute.BlueBrick,
+    Icon = "Soneta.Examples.Utils.examples.ico;Soneta.Examples",
+    Contexts = new object[] { LicencjeModułu.All }
+)]
 
 [assembly: FolderView("Soneta.Examples/Rozwiazania zadania rekrutacyjnego",
     Priority = 13,
