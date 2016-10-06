9693560cb67cd09f4c6b1c9fc5202a11a2e71226
Michał Domeredzki <michal.d85@gmail.com>
2016-10-06 21:02:03 +0200

    wiecej testow

--- Soneta.Tests/Tests/Tests.cs
+++ Soneta.Tests/Tests/Tests.cs
@@ -22,6 +22,8 @@ namespace Soneta.Examples.Rozwiazanie.Tests
             _gitHubService = new GitHubService();
         }
 
+        // prawidlowa agregacja
+
         [Test]
         public void Test1()
         {
@@ -53,6 +55,122 @@ namespace Soneta.Examples.Rozwiazanie.Tests
          
             var result = _gitHubService.GetDailyStatistics(commits);
             Assert.AreEqual(result.Count(), 0);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test1", Name = "test1" });
+
+            var result = _gitHubService.GetDailyStatistics(commits);
+            Assert.AreEqual(result.Count(), 2);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test2", Name = "test2" });
+
+            var result = _gitHubService.GetDailyStatistics(commits);
+            Assert.AreEqual(result.Count(), 3);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test2", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2", Name = "test2" });
+
+            var result = _gitHubService.GetDailyStatistics(commits);
+            Assert.AreEqual(result.Count(), 4);
+        }
+
+        [Test]
+        public void Test7()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test1", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test2", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2", Name = "test2" });
+
+            var result = _gitHubService.GetDailyStatistics(commits);
+            Assert.AreEqual(result.Count(), 4);
+        }
+
+        // liczba commitow
+
+        [Test]
+        public void Test8()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test1@op.pl", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test1@op.pl", Name = "test1" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+
+            var result = _gitHubService.GetDailyStatistics(commits).ToList();
+            Assert.AreEqual(result[0].Author, "test1@op.pl");
+            Assert.AreEqual(result[0].CommitsCount, 1);
+
+            Assert.AreEqual(result[1].Author, "test2@op.pl");
+            Assert.AreEqual(result[1].CommitsCount, 2);
+
+            Assert.AreEqual(result[2].Author, "test1@op.pl");
+            Assert.AreEqual(result[2].CommitsCount, 1);
+
+            Assert.AreEqual(result[3].Author, "test2@op.pl");
+            Assert.AreEqual(result[3].CommitsCount, 1);
+        }
+
+        [Test]
+        public void Test9()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+
+
+            var result = _gitHubService.GetDailyStatistics(commits).ToList();
+            Assert.AreEqual(result[0].Author, "test2@op.pl");
+            Assert.AreEqual(result[0].CommitsCount, 4);
+        }
+
+        // sortowanie po datcie
+        [Test]
+        public void Test10()
+        {
+            // testowe dane
+            var commits = new List<Author>();
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 09), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 08), Email = "test2@op.pl", Name = "test2" });
+            commits.Add(new Author { DateTime = new DateTime(2016, 10, 10), Email = "test2@op.pl", Name = "test2" });
+        
+
+            var result = _gitHubService.GetDailyStatistics(commits).ToList();
+            Assert.AreEqual(result.Count(), 3);
+            Assert.AreEqual(result[0].Date, "10.10.2016");
+            Assert.AreEqual(result[1].Date, "09.10.2016");
+            Assert.AreEqual(result[2].Date, "08.10.2016");
+
         }
     }
 }
